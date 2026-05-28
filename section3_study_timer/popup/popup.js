fb5bbc6a67b7b9eb79d7cd2d1b5112f81fcb168f
jonaskwan88-code <jonastheitguy@gmail.com>
2026-05-28 18:30:40 +0800

    nice

--- section3_study_timer/popup/popup.js
+++ section3_study_timer/popup/popup.js
@@ -1,36 +1,54 @@
 let tasks = []
 
 const addTaskBtn = document.getElementById("add-task-btn")
-addTaskBtn.addEventListener("click", ()=>addTask())
+addTaskBtn.addEventListener("click", () => addTask())
+
+chrome.storage.sync.get(["tasks"], (res) => {
+    tasks = res.tasks ? res.tasks : []
+    renderTasks()
+})
+
+function saveTasks() {
+    chrome.storage.sync.set({
+        tasks,
+    })
+}
+
+function renderTask(taskNum) {
+    const taskRow = document.createElement("div")
+    const text = document.createElement("input")
+    text.type = "text"
+    text.placeholder = "Enter a task..."
+    text.value = tasks[taskNum]
+    text.className = "task-input"
+    text.addEventListener("change", () => {
+        tasks[taskNum] = text.value
+        saveTasks()
+    })
+    const deleteBtn = document.createElement("input")
+    deleteBtn.type = "button"
+    deleteBtn.value = "X"
+    deleteBtn.addEventListener("click", () => {
+      
+    })
+
+    taskRow.appendChild(text)
+    taskRow.appendChild(deleteBtn)
+
+    const taskContainer = document.getElementById("task-container")
+    taskContainer.appendChild(taskRow)
+}
 
 
+function addTask() {
+    const taskNum = tasks.length
+    tasks.push("")
+    renderTask(taskNum)
+    saveTasks()
+}
 
-
-function addTask(){
-  const taskNum = tasks.length
-  tasks.push("")
-  
-  const taskRow = document.createElement("div")
-  const text = document.createElement("input")
-  text.type = "text"
-  text.placeholder = "Enter a task..."
-  text.addEventListener("change", ()=>{
-    tasks[taskNum] = text.value
-  })
-  const deleteBtn = document.createElement("input")
-  deleteBtn.type = "button"
-  deleteBtn.value = "X"
-  deleteBtn.addEventListener("click", ()=>{
-    tasks.splice(taskNum,1)
-    taskRow.remove()
-  })
-
-  taskRow.appendChild(text)
-  taskRow.appendChild(deleteBtn)
-
-  const taskContainer = document.getElementById("task-container")
-  taskContainer.appendChild(taskRow)
-
-
-
+function deleteTask(taskNum){
+    tasks.splice(taskNum, 1)
+    renderTasks()
+    saveTasks()
 }
